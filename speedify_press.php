c9de629d4e840af35179be7be805a2929ec3f7b8
Leon Chevalier <leon@aciddrop.com>
2025-06-10 15:47:28 +0200

    Licensing hotfixes

--- speedify_press.php
+++ speedify_press.php
@@ -3,7 +3,7 @@
 Plugin Name: SpeedifyPress
 Description: Suite of tools and utilities to optimise WordPress sites
 Author: Leon Chevalier
-Version: 0.57.0
+Version: 0.57.1
 Text Domain: speedify-press
 Author URI: https://github.com/acid-drop
 */
