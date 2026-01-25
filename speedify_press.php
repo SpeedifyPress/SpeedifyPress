a5c2018856acd30f100172139259eaaa300f1539
Leon Chevalier <leon@aciddrop.com>
2026-01-25 13:39:01 +0100

    Fix bug in calling is_shop

--- speedify_press.php
+++ speedify_press.php
@@ -3,7 +3,7 @@
 Plugin Name: SpeedifyPress
 Description: Suite of tools and utilities to optimise WordPress sites
 Author: Leon Chevalier
-Version: 0.80.03
+Version: 0.80.04
 Text Domain: speedify-press
 Author URI: https://github.com/acid-drop
 */
